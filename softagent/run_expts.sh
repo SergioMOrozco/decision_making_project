1ab17e6ed8443df2769e745e4634290a3eca2205
Mr-vedant-gupta <91429389+Mr-vedant-gupta@users.noreply.github.com>
2024-04-28 21:07:45 -0400

    Update run_expts.sh

--- softagent/run_expts.sh
+++ softagent/run_expts.sh
@@ -1,6 +1,6 @@
-#python experiments/run_planet.py --env_name ClothFlatten --log_dir ./data/clothflatten_seed100 --seed 100 --env_kwargs_num_variations 1000 --train_epoch 10 --test_interval 1
-python experiments/run_planet.py --env_name RopeFlatten --log_dir ./data/ropeflatten_seed100 --seed 100 --env_kwargs_num_variations 1000 --train_epoch 5 --test_interval 1
-python experiments/run_planet.py --env_name ClothFold --log_dir ./data/clothfold_seed100 --seed 100 --env_kwargs_num_variations 1000 --train_epoch 5 --test_interval 1
-python experiments/run_planet.py --env_name ClothFlatten --log_dir ./data/clothflatten_seed110 --seed 110 --env_kwargs_num_variations 1000 --train_epoch 5 --test_interval 1
-python experiments/run_planet.py --env_name RopeFlatten --log_dir ./data/ropeflatten_seed110 --seed 110 --env_kwargs_num_variations 1000 --train_epoch 5 --test_interval 1
-python experiments/run_planet.py --env_name ClothFold --log_dir ./data/clothfold_seed110 --seed 110 --env_kwargs_num_variations 1000 --train_epoch 5 --test_interval 1
+python experiments/run_planet.py --env_name ClothFlatten --log_dir ./data/clothflatten_seed100 --seed 100 --env_kwargs_num_variations 1000 --train_epoch 10 --test_interval 1
+python experiments/run_planet.py --env_name RopeFlatten --log_dir ./data/ropeflatten_seed100 --seed 100 --env_kwargs_num_variations 1000 --train_epoch 10 --test_interval 1
+python experiments/run_planet.py --env_name ClothFold --log_dir ./data/clothfold_seed100 --seed 100 --env_kwargs_num_variations 1000 --train_epoch 10 --test_interval 1
+python experiments/run_planet.py --env_name ClothFlatten --log_dir ./data/clothflatten_seed110 --seed 110 --env_kwargs_num_variations 1000 --train_epoch 10 --test_interval 1
+python experiments/run_planet.py --env_name RopeFlatten --log_dir ./data/ropeflatten_seed110 --seed 110 --env_kwargs_num_variations 1000 --train_epoch 10 --test_interval 1
+python experiments/run_planet.py --env_name ClothFold --log_dir ./data/clothfold_seed110 --seed 110 --env_kwargs_num_variations 1000 --train_epoch 10 --test_interval 1
